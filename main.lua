f29ba73fc7dfb232c0ff2843f6c997f6a8c053a3
Keffori <90979483+Keffori@users.noreply.github.com>
2023-04-24 16:25:23 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@ if not IY_LOADED then
 end
 
 local IS_Settings = {
-	["Version"] = "1.0.7",
+	["Version"] = "1.0.8",
 	["Invite"] = "SkAneeutak",
 	["Plugins"] = LoadUrl("https://raw.githubusercontent.com/Keffori/Infinite-Store/main/sources.luau"),
 	["NsfwPlugins"] = LoadUrl(
@@ -21,7 +21,7 @@ local IS_Settings = {
 	),
 }
 
-local ann = "Update 1.0.7, see changelog in discord"
+local ann = "Update 1.0.8, see changelog in discord"
 local _UserSettings = {
 	["StartMinimized"] = false,
 	["SafeMode"] = true,
